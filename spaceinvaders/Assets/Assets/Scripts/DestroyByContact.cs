22117055c91c17cc2c1c5951a79783abc32cfa3a
grandnumber <thegrandnumber@gmail.com>
2016-06-23 16:11:05 -0700

    add asteroid, boundary and destroy

---
+++ spaceinvaders/Assets/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+
+	public GameObject explosion; 
+	public GameObject playerExplosion; 
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.CompareTag ("boundary")) {
+			return;
+		}
+		Instantiate (explosion, transform.position, transform.rotation);
+
+		if (other.gameObject.CompareTag ("boundary")) {
+			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
+		}
+			Destroy(other.gameObject); 
+			Destroy(gameObject); 
+		
+	}
+
+	}
