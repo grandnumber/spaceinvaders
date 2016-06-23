a83d88232492e698e4a55cc875d0a1726ba77b51
grandnumber <thegrandnumber@gmail.com>
2016-06-23 13:43:47 -0700

    add player move & tilt

---
+++ spaceinvaders/Assets/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary {
+	public float xMin, xMax, zMin, zMax; 
+}
+
+public class PlayerController : MonoBehaviour {
+	private Rigidbody rb; 
+	private Vector3 direction; 
+	public float speed; 
+	public Boundary boundary; 
+	public float tilt; 
+
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		float moveH = Input.GetAxis ("Horizontal");
+		float moveV = Input.GetAxis ("Vertical");
+
+		direction = new Vector3 (moveH, 0.0f, moveV);
+		rb.velocity = direction * speed; 
+
+
+		rb.position = new Vector3 
+			(
+				Mathf.Clamp (rb.position.x, boundary.xMin, boundary.xMax), 
+				0.0f, 
+				Mathf.Clamp (rb.position.z, boundary.zMin, boundary.zMax)
+			);
+
+		rb.rotation = Quaternion.Euler (0.0f, 0.0f, rb.velocity.x * -tilt);
+	}
+}
