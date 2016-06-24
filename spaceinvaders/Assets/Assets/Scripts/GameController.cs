511d728d25db62e665b90bca6568d777386547d2
grandnumber <thegrandnumber@gmail.com>
2016-06-23 17:42:19 -0700

    add wave & spawn asteroids

---
+++ spaceinvaders/Assets/Assets/Scripts/GameController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	
+	public GameObject hazard;
+	public Vector3 spawnValues;
+	public Quaternion spawnRotation; 
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (spawnWaves ());
+	}
+
+	IEnumerator spawnWaves (){
+		yield return new WaitForSeconds (startWait);
+		while (true){
+			
+			for( int i=0; i< hazardCount; i++){
+					
+			Vector3 spawnPosition  = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+			Quaternion spawnRotation =  Quaternion.identity; 
+			Instantiate (hazard, spawnPosition, spawnRotation); 
+			yield return new WaitForSeconds (spawnWait);
+		}
+			yield return new WaitForSeconds (waveWait); 
+	}
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
